826f45d1a7c316a21cb9e44ff154b74b866c1cc6
Eduardo54992506 <Eduardo54992506@edu.df.senac.br>
2025-04-03 10:45:38 -0300

    confirmado alterações

---
+++ Atividade 1/Program.cs
@@ -0,0 +1,19 @@
+﻿class program
+{
+    public static void Main()
+    {
+
+        Console.WriteLine("informe um numero: ");
+        int num1 = Convert.ToInt32(Console.ReadLine());
+
+        if (num1 / 2)
+        {
+            Console.WriteLine("O nunmero é par!");
+        }
+
+        else
+        {
+            Console.WriteLine("O numero é impar!");
+        }
+    }
+}
