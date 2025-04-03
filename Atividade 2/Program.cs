16ef80a2d07e30baee1bb94f1418ee8a5ab60494
Eduardo54992506 <Eduardo54992506@edu.df.senac.br>
2025-04-03 11:20:55 -0300

    atividade 2

--- Atividade 2/Program.cs
+++ Atividade 2/Program.cs
@@ -2,6 +2,18 @@
 {
     public static void Main()
     {
+        Console.WriteLine("Escreva sua idade: ");
+        int idade = Convert.ToInt32( Console.ReadLine());
+        
+        if (idade <= 17)
+        {
+            Console.WriteLine("Menor de idade!");
+        }
 
+        else
+        {
+            Console.WriteLine("maior de idade!");
+        }
+   
     }
 }
