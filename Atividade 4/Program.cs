314673774d0ec46da2b3d6cff0ebcb97feea314a
Eduardo54992506 <Eduardo54992506@edu.df.senac.br>
2025-04-07 08:20:04 -0300

    questão 4 e 6

--- Atividade 4/Program.cs
+++ Atividade 4/Program.cs
@@ -1,48 +1,22 @@
-﻿
-
-using Microsoft.VisualBasic;
-
-class program
+﻿class program
 {
-
     public static void Main()
     {
+        Console.WriteLine("digite um numero de 1 a 7");
+        int num = Convert.ToInt32( Console.ReadLine());
 
+        switch (num)
+        {
+            case 1: Console.WriteLine("domingo"); break;
+            case 2: Console.WriteLine("segunda - feira"); break;
+            case 3: Console.WriteLine("terça - feira"); break;
+            case 4: Console.WriteLine("quarta - feira"); break;
+            case 5: Console.WriteLine("quinta - feira"); break;
+            case 6: Console.WriteLine("sexta - feira"); break;
+            case 7: Console.WriteLine("sabado"); break;
+            default: Console.WriteLine("numero invalido"); break;
+        }
 
         
-{
-    let message; 
-    
-   
-    {
-    case 'segunda-feira':
-        message = 'Comece a semana com energia!';
-        break;
-
-    case 'terça-feira':
-        message = 'Vamos continuar a semana com foco!';
-        break;
-
-    case 'quarta-feira':
-        message = 'Metade da semana já foi!';
-        break;
-
-    case 'quinta-feira':
-        message = 'Quase lá, continue firme!';
-        break;
-
-    case 'sexta-feira':
-        message = 'É hora de relaxar, o final de semana está chegando!';
-        break;
-
-    case 'sábado':
-        message = 'Aproveite o seu sábado!';
-        break;
-
-    case 'domingo':
-        message = 'Dia de descanso e preparação para a semana!';
-        break;
-                    ]
-            }
-}  
-            
+    }
+}
--- Atividade 6/Program.cs
+++ Atividade 6/Program.cs
@@ -7,34 +7,37 @@
         Console.WriteLine("2 - Subtração");
         Console.WriteLine("3 - Multiplicação");
         Console.WriteLine("4 - Divisão");
-        int num1 = Convert.ToInt32(Console.ReadLine());
+        char num1 = Convert.ToChar(Console.ReadLine());
 
-        Console.WriteLine("digite um número:");
+        Console.WriteLine("digite número 1:");
         int num2 = Convert.ToInt32( Console.ReadLine());
 
-        Console.WriteLine("digite um número:");
+        Console.WriteLine("digite número 2:");
         int num3 = Convert.ToInt32(Console.ReadLine());
 
 
-        if (num1 == 1)
+        if (num1 == '1')
         {
             int soma = num2 + num3;
+            Console.WriteLine("Resultado da operação");
             Console.WriteLine(soma);
         }
-        else if (num1 != num3)
+        else if (num1  == '2')
         {
             int subtração = num2 - num3;
-
+            Console.WriteLine("Resultado da operação");
             Console.WriteLine(subtração);
         }
-        else if (num1 != num3)
+        else if (num1 == '3')
         {
             int multiplicação = num2 * num3;
+            Console.WriteLine("Resultado da operação");
             Console.WriteLine(multiplicação);
         }
-        else if (num1 != num3)
+        else if (num1 == '4')
         {
             int divisão = num2 / num3;
+            Console.WriteLine("Resultado da operação");
             Console.WriteLine(divisão);
         }
     }
