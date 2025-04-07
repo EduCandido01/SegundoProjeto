c6f195c51da9c270577f39a3554aecf8a64dee7b
Eduardo54992506 <Eduardo54992506@edu.df.senac.br>
2025-04-07 08:46:33 -0300

    Projeto finalizado

---
+++ atividade adicional/Program.cs
@@ -0,0 +1,33 @@
+﻿class program
+{
+    public static void Main()
+    {
+        Console.WriteLine("Coloque o primeiro número: ");
+        int num1 = Convert.ToInt32(Console.ReadLine());
+
+        Console.WriteLine("Coloque o primeiro número: ");
+        int num2 = Convert.ToInt32(Console.ReadLine());
+
+        Console.WriteLine("1 - soma: ");
+        Console.WriteLine("2 - subtração: ");
+        Console.WriteLine("3 - multiplicação: ");
+        Console.WriteLine("4 - divisão: ");
+        char opcao = Convert.ToChar(Console.ReadLine());
+
+        switch (opcao)
+        {
+            case '1': Console.WriteLine("resultado: " + (num1 + num2));
+                break;
+            case '2': Console.WriteLine("resultado: " + (num1 - num2));
+                break;
+            case '3': Console.WriteLine("resultado: " + (num1 * num2));
+                break;
+            case '4': Console.WriteLine("resultado: " + (num1 / num2));
+                break;
+            default: Console.WriteLine("Erro de digitação"); 
+                break;
+        }
+
+
+    }
+}
